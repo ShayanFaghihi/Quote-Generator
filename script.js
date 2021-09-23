ec0e9b0b874fae3c282f5ce51ffa8202fbcdfa1d
Shayan.Faghihi <shayan.faghihi93@gmail.com>
2021-09-23 19:00:32 +0330

    DOM connected to the app

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
+const quote = document.getElementById("quote");
+const author = document.getElementById("quoteAuthor");
+const btn = document.getElementById("quoteBtn");
+
 async function getQuote() {
   const proxy = "https://cors-anywhere.herokuapp.com/";
+
   const api_Url =
     "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
 
   try {
-    const request = await fetch(proxy + api_Url);
+    const request = await fetch(api_Url);
     const response = await request.json();
 
-    console.log(response);
+    quote.innerHTML = response.quoteText;
+    author.innerHTML = response.quoteAuthor;
   } catch (error) {
+    getQuote();
     console.log("Whoops, no quote to show", error);
   }
 }
 getQuote();
+
+btn.addEventListener("click", getQuote);
