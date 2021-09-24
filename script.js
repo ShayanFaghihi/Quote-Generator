ebf2c5345750a2e058c2b5aa72382a67823e86d7
Shayan.Faghihi <shayan.faghihi93@gmail.com>
2021-09-24 10:03:08 +0330

    Twitter btn added

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
+const quoteContainer = document.querySelector('.quote-container')
 const quote = document.getElementById("quote");
 const author = document.getElementById("quoteAuthor");
 const btn = document.getElementById("quoteBtn");
+const shareBtn = document.querySelector('.twitter');
+const loader = document.querySelector('.loader');
 
 async function getQuote() {
+  // using proxy to bypass the CORS
   const proxy = "https://cors-anywhere.herokuapp.com/";
 
   const api_Url =
@@ -12,13 +16,25 @@ async function getQuote() {
     const request = await fetch(api_Url);
     const response = await request.json();
 
-    quote.innerHTML = response.quoteText;
-    author.innerHTML = response.quoteAuthor;
+    // Showing Loader before showing the quote
+    if(response.quoteText.length > 0) {
+      loader.classList.add('hidden');
+      quoteContainer.classList.remove('hidden');
+      quote.innerHTML = response.quoteText;
+      author.innerHTML = response.quoteAuthor ? response.quoteAuthor : 'Unknown';
+    } else {
+      loader.classList.remove('hidden');
+      quoteContainer.classList.add('hidden');
+    }
   } catch (error) {
     getQuote();
-    console.log("Whoops, no quote to show", error);
   }
 }
 getQuote();
 
 btn.addEventListener("click", getQuote);
+
+// Twitter Share Button
+shareBtn.addEventListener('click', () => {
+  window.location.href = `https://twitter.com/intent/tweet?text=${quote.innerText}`;
+})
